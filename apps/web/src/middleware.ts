61c7b72d7ef1897016930646749c88c9e8b47e94
Rahul <rahulmadagoud16@gmail.com>
2026-06-25 21:34:28 +0530

    Middleware File Created

---
+++ apps/web/src/middleware.ts
@@ -0,0 +1,15 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+const isPublic = createRouteMatcher([
+  '/',
+  '/sign-in(.*)',
+  '/sign-up(.*)',
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (!isPublic(req)) auth().protect();
+});
+
+export const config = {
+  matcher: ['/((?!_next|.*\\..*).*)'],
+};
